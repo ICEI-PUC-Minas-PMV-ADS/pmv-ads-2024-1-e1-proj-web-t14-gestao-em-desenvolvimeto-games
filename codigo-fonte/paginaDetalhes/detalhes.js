7ab45ca864ad57da60611a7b2fd58db7b2140f97
Roger Marques <marquesroger226@gmail.com>
2024-04-27 16:54:50 -0300

    feat: Renderizando os detalhes do ativo na tela

---
+++ codigo-fonte/paginaDetalhes/detalhes.js
@@ -0,0 +1,33 @@
+/* Mapeamento dos detalhes do ativo digital */
+
+const active = {
+    id: '1',
+    title: 'Pacote Low poly Nature 1',
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+    molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+    numquam blanditiis harum`,
+    status: 'Finalizado',
+    img: 'https://img.itch.zone/aW1hZ2UvMTE0NTkzLzUzMDgwNy5qcGc=/original/%2F0ce0N.jpg',
+}
+
+const activeSection = document.querySelector('.active-details')
+
+const activeImg = document.createElement('img')
+activeImg.src = active.img
+activeImg.className = 'active-img'
+
+const activeTitle = document.createElement('h1')
+activeTitle.innerHTML = active.title
+activeTitle.className = 'active-title'
+
+const activeDesc = document.createElement('p')
+activeDesc.innerHTML = active.description
+activeDesc.className = 'active-desc'
+
+activeSection.appendChild(activeImg)
+activeSection.appendChild(activeTitle)
+activeSection.appendChild(activeDesc)
+
+
+
+
