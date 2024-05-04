463f74c4722507937d8d98b2277e2077e776de4e
Roger Marques <marquesroger226@gmail.com>
2024-05-04 13:14:27 -0300

    feat: Adicionando a possibilidade de filtrar os ativos por suas categorias

--- codigo-fonte/paginaHome/home.js
+++ codigo-fonte/paginaHome/home.js
@@ -1,28 +1,42 @@
 /* Mapeamento de categorias */
+const actives = JSON.parse(localStorage.getItem('active-list'))
 
 const categories = ['2D', '3D', 'Animações', 'Texturas']
 
 const categoriesList = document.querySelector('.categories-list')
 
+const activesList = document.querySelector('.actives-list')
+
+const emptyListContainer = document.querySelector('.empyt-list-container')
+
+//Seleção de categorias
+
+function selectCategory() {
+    const filteredActives =actives.filter((active) => active.category.
+        includes(event.target.innerHTML.toLowerCase()))
+
+    activesList.innerHTML = ""
+    
+    if(filteredActives.length === 0 ) return emptyListContainer.classList.add('visible')
+
+    emptyListContainer.classList.remove('visible')
+    renderActives(filteredActives)
+}
+
+//Mapeamento de categorias
+
 categories.forEach((category) => {
     const li = document.createElement('li')
     li.innerHTML = category
     li.className = 'categories-card'
+    li.addEventListener('click', selectCategory)
 
     categoriesList.appendChild(li)
 })
 
 /* Mapeamento dos ativos digitais */
 
-const actives = JSON.parse(localStorage.getItem('active-list'))
-
-const activesList = document.querySelector('.actives-list')
-
-if(!actives || actives.length === 0) {
-    const emptyListContainer = document.querySelector('.empyt-list-container')
-
-    emptyListContainer.classList.add('visible')
-} else {
+function renderActives(actives) {
     actives.forEach((active) => {
         const li = document.createElement('li')
         li.className = 'active-card'
@@ -71,11 +85,22 @@ if(!actives || actives.length === 0) {
     })
 }
 
+//Renderizando a mensagem caso a lista de ativos esteja vazia
+
+if(!actives || actives.length === 0) {
+    emptyListContainer.classList.add('visible')
+} else {
+    renderActives(actives)
+}
+
+//Função de redirecionamento para o perfil
+
 function goToProfile() {
     window.location.href = "../paginaPerfil/perfil.html"
 }
 
+//Função de redirecionamento para a página do ativo
+
 function goToAddActivePage() {
     window.location.href = "../paginaAdicionarAtivos/adicionarAtivos.html"
 }
-
